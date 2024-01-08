29ba67956d1242d6e5db0c649021cbebc9dc751d
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-01-08 17:13:30 +0100

    Modify ExecuteShortcut method in ShortcutClient.cs
    Changed the method to accept also profileName parameter.
    Added null check for grpcAddress from configuration.
    Added exception handling for calling server app, because it was crashing when the server coudn't be found.

--- Shortcuts.Web/ShortcutsClient.cs
+++ Shortcuts.Web/ShortcutsClient.cs
@@ -13,42 +13,32 @@ public class ShortcutsClient
         _config = config;
         _logger = logger;
     }
-
+    public void ExecuteShortcut(string profileName, string shortcutName)
     {
         _logger.LogInformation("Executing shortcut: {profileName}: {shortcutName}", profileName, shortcutName);
-    }
-
-    {
+        var grpcAddress = _config.GetValue<string>("Grpc:Shortcuts");
+        if (string.IsNullOrWhiteSpace(grpcAddress))
+        {
             _logger.LogError("Grpc:Shortcuts address not provided. Shortcut execution terminated.");
-    }
-
-        ShortcutRequest request = new ShortcutRequest
-    {
-            ProcessName = _config.GetValue<string>($"ShortcutProfiles:{profileName}:ProcessName"),
-    }
-
-        _logger.LogInformation("Shortcut request: {ProcessName}, {KeyStroke}", request.ProcessName, request.KeyStroke);
-    {
-            client.ExecuteShortcut(request);
-    }
-
-    {
-            _logger.LogError("Error while calling server (address: {grpcAddress})\n{ex.Message}", grpcAddress, ex.Message);
-    }
-
-    private void ExecuteShortcut(string shortcutName)
-    {
-        _logger.LogInformation("Executing shortcut: {shortcutName}", shortcutName);
-        var channel = GrpcChannel.ForAddress(_config.GetValue<string>("Grpc:Shortcuts"));
-        var client = new Shortcuts.Server.Shortcut.ShortcutClient(channel);
+            return;
+        }
+        var channel = GrpcChannel.ForAddress(grpcAddress);
+        var client = new Server.Shortcut.ShortcutClient(channel);
 
         ShortcutRequest request = new ShortcutRequest
         {
-            ProcessName = _config.GetValue<string>($"ShortcutProfiles:{_profile}:ProcessName"),
-            KeyStroke = _config.GetValue<string>($"ShortcutProfiles:{_profile}:Shortcuts:{shortcutName}")
+            ProcessName = _config.GetValue<string>($"ShortcutProfiles:{profileName}:ProcessName"),
+            KeyStroke = _config.GetValue<string>($"ShortcutProfiles:{profileName}:Shortcuts:{shortcutName}")
         };
 
-        _logger.LogInformation($"Shortcut request: {request.ProcessName}, {request.KeyStroke}");
-        client.ExecuteShortcut(request);
+        _logger.LogInformation("Shortcut request: {ProcessName}, {KeyStroke}", request.ProcessName, request.KeyStroke);
+        try
+        {
+            client.ExecuteShortcut(request);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Error while calling server (address: {grpcAddress})\n{ex.Message}", grpcAddress, ex.Message);
+        }
     }
 }
