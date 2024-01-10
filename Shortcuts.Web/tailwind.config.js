d5d42de78455a149e6d0565e09498a3ddd194d14
Adam Monikowski <adam.piotr.monikowski@gmail.com>
2024-01-10 22:05:02 +0100

    Style pages and components using Tailwind CSS

--- Shortcuts.Web/tailwind.config.js
+++ Shortcuts.Web/tailwind.config.js
@@ -1,7 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./**/*.{razor,html}'],
-  theme: {
+    safelist: [
+        {
+            pattern: /grid-cols-+/
+        },
+        {
+            pattern: /col-span-+/
+        },
+        {
+            pattern: /bg-+/
+        },
+        {
+            pattern: /from-+/
+        },
+        {
+            pattern: /to-+/
+        }
+    ],
+    theme: {
+        container: {
+            center: true,
+            padding: "2rem"
+        },
     extend: {},
   },
   plugins: [],
